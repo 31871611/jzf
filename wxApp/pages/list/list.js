feba9bfc8cda5999cf51ac0b5f438623bdd58907
btname <btname@163.com>
2017-01-11 17:47:38 +0800

    master wxapp

---
+++ wxApp/pages/list/list.js
@@ -0,0 +1,25 @@
+// pages/list/list.js
+Page({
+  data:{
+    msg : '列表',
+    data : 10,
+    list:{
+      photo : '../../public/images/230x230.jpg'
+    }
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
