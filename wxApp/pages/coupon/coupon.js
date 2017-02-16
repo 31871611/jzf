1486ceeb94c396840ec8140dce068464b21b86ff
31871611 <31871611@qq.com>
2017-02-16 23:15:43 +0800

    master 购买弹窗

--- wxApp/pages/coupon/coupon.js
+++ wxApp/pages/coupon/coupon.js
@@ -1,6 +1,8 @@
 // pages/coupon/coupon.js
 Page({
   data:{
+    className:'',
+    isAlert:true,
     list:{
       photo : '../../public/images/230x230.jpg'
     }
@@ -16,8 +18,35 @@ Page({
   },
   onHide:function(){
     // 页面隐藏
+
+    // 关闭弹窗
+    this.setData({
+      isAlert:true,
+      className:'SlideTopBottom-leave'
+    })
   },
   onUnload:function(){
     // 页面关闭
+
+  },
+  // 显示弹窗
+  showAlert:function(){
+      this.setData({
+        isAlert:false,
+        className:'SlideTopBottom-enter'
+      })
+  },
+  // 关闭弹窗
+  exitAlert:function(){
+    var _this = this;
+    this.setData({
+      className:'SlideTopBottom-leave'
+    })
+    setTimeout(function(){
+      _this.setData({
+        isAlert:true,
+        className:''
+      })
+    },400)
   }
 })
